a33e82610c8e949882ab45eef5522b365d9d8fda
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2020-11-04 18:16:48 +0100

    Updated default launcher to run the node

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,10 +13,10 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
-
 dt-exec Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
 export DISPLAY=:1
+
+dt-exec rosrun dtsim-ros-wrapper dtsim-ros-wrapper.py
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
 
